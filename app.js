e54296ee533501ea6c7c8a166191c795d8ce64a3
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-08 00:25:40 +0530

    done

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
-
-
+const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+main().then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
 app.get('/', (req, res) => {
   res.send('Hello I am root');
 });
