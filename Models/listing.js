dc2ccba4898f0c0ac397681d01046e432649d122
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-08 16:16:32 +0530

    done

--- Models/listing.js
+++ Models/listing.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const listingSchema = new Schema({
-    title: String, 
-    description: String,
-    imagine: String, 
-    price: Number,
-    location: String, 
-    Country: String
+    title: {
+        type: String, 
+        required: true
+    }, 
+    description: {
+        type: String, 
+        required: true
+    },
+    imagine: {
+        type: String, 
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String, 
+        required: true
+    },
+    Country: {
+        type: String,
+        required: true
+    }
 });
 const Listing= mongoose.model('Listing', listingSchema); 
+module.exports = Listing;
