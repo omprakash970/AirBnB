7613aa50d9f5b16fad2904bfa82116966a806e0c
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-23 23:43:19 +0530

    done

--- app.js
+++ app.js
@@ -8,15 +8,16 @@
 // app.set('views', path.join(__dirname, 'views'));
 // app.use(express.static(path.join(__dirname, 'public')));
 const express = require('express');
-const app = express();
-const port =3000; 
-const mongoose=require('mongoose');
-const Listing=require('./Models/listing'); 
+const mongoose = require('mongoose');
 const path = require('path');
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); 
-app.use(express.static(path.join(__dirname, 'public'))); 
+const Listing = require('./Models/listing');
 
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 
 const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
@@ -31,22 +32,36 @@ main().then(() => {
 app.get('/', (req, res) => {
   res.send('Hello I am root');
 });
-app.get('/listings', async(req, res)=>{
+app.get('/listings', async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("listings/index", { listings: allListings });
+  res.render('listings/index', { listings: allListings });
 });
-app.use(express.urlencoded({ extended: true }));
 //show route: 
-app.get('/listings/new', (req, res)=>{
-  res.render("listings/new.ejs");
-  
+app.get('/listings/new', (req, res) => {
+  res.render('listings/new.ejs');
+});
+app.get('/listings/:id', async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    return res.status(404).send('Listing not found');
+  }
+  res.render('listings/show', { listing });
+});
 
-})
-app.get('/listings/:id', async(req, res)=>{
-  const {id} = req.params;
-  const listing= await Listing.findById(id); 
-  res.render("listings/show", { listing }); 
-})
+app.post('/listings', async (req, res) => {
+  const { title, description, price, location, country } = req.body;
+  const newListing = new Listing({
+    title,
+    description,
+    image: "",
+    price,
+    location,
+    Country: country
+  });
+  await newListing.save();
+  res.redirect(`/listings/${newListing._id}`);
+});
 
 // app.get('/testListing',async (req, res)=>{
 //   let sampleListing = new Listing({
