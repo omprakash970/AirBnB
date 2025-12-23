f8568d1dc322afeb1f3381fb1454b30e4a7a8c6e
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-23 23:13:35 +0530

    done

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-const data = require('./data');
+const { data } = require('./data');
 const Listing=require('../Models/listing'); 
 const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
 main()
