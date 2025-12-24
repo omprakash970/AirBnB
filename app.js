24c3386279bb9a67d8f0c9da8e29288601cb46d9
Bandi Om Prakash <omprakashbandi583@gmail.com>
2025-12-24 21:31:38 +0530

    done

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Listing = require('./Models/listing');
+const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
 
 const app = express();
@@ -20,6 +21,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(expressLayouts);
+app.set('layout', 'Layouts/boilerplate');
 
 
 const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
